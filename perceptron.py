e020982938b406c25c37e6b479ce32f30ea5cdc5
Will Whitney <wwhitney@mit.edu>
2013-02-24 12:57:49 -0500

    perceptron done, complete with vector orientation bullshit

--- perceptron.py
+++ perceptron.py
@@ -1,26 +1,37 @@
 import numpy as np
 from project1 import *
+
 def perceptron(X, Y, numEpochs):
     theta = np.zeros(X[1].size)
-    theta0 = 0
-    thetas = [(theta, theta0)]
+    thetas = [theta]
 
     epochs = 0
     while epochs < numEpochs:
         missed = False
         for i, row in enumerate(X):
-            comparator = np.dot(theta, row) + theta0
+            comparator = np.dot(theta, row)
             if np.sign(comparator) != Y[i]:
                 missed = True
                 theta = theta + Y[i] * X[i]
-                theta0 = theta0 + Y[i]
-                thetas.append((theta, theta0))
+                thetas.append(theta)
 
-            print thetas
         epochs += 1
         if missed == False:
-            break
+            result = np.array(theta)[np.newaxis]
+            return result.T
 
+    options = []
+    for theta in thetas:
+        misses = 0
+        for i, row in enumerate(X):
+            comparator = np.dot(theta, row)
+            if np.sign(comparator) != Y[i]:
+                misses +=1
 
+        options.append((misses, theta))
+
+    options.sort(key=lambda option: option[0])
+    result = np.array(options[0][1])[np.newaxis]
+    return result.T
 
 perceptron(X1, Y1, 2)
